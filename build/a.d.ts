933751a88ae91440d32ad78a69ffdc890c79834b
avo <avetik.sukiasyan@metamap.com>
2024-10-29 11:43:46 +0400

    update version to 2.4.11

--- build/a.d.ts
+++
@@ -1,1 +0,0 @@
-//# sourceMappingURL=a.d.ts.map
--- ios/MetaMapRNSdk.m
+++ ios/MetaMapRNSdk.m
@@ -1,34 +1,70 @@
 #import "MetaMapRNSdk.h"
 
-
 @implementation MetaMapRNSdk {
-    bool hasListeners;
+    BOOL hasListeners;
 }
 
 RCT_EXPORT_MODULE()
 
-RCT_EXPORT_METHOD(showFlow:(NSString * _Nonnull)clientId flowId:(NSString * _Nullable)flowId metadata:(NSDictionary<NSString *, id> * _Nullable)metadata)
+RCT_EXPORT_METHOD(showFlow:(NSString * _Nonnull)clientId
+                  flowId:(NSString * _Nullable)flowId
+                  metadata:(NSDictionary<NSString *, id> * _Nullable)metadata)
 {
-    dispatch_async(dispatch_get_main_queue(), ^(void){
-        NSMutableDictionary *mutableDictionary = [metadata mutableCopy];     //Make the dictionary mutable to change/add
-        mutableDictionary[@"sdkType"] = @"react-native-ios";
-        [MetaMap.shared showMetaMapFlowWithClientId: clientId flowId: flowId configurationId: nil encryptionConfigurationId: nil metadata: metadata];
-        [MetaMapButtonResult shared].delegate = self;
+    dispatch_async(dispatch_get_main_queue(), ^{
+        NSMutableDictionary *mutableMetadata = [metadata mutableCopy] ?: [NSMutableDictionary new];
+        mutableMetadata[@"sdkType"] = @"react-native-ios";
+        
+        [MetaMap.shared showMetaMapFlowWithClientId:clientId
+                                             flowId:flowId
+                                    configurationId:nil
+                          encryptionConfigurationId:nil
+                                           metadata:mutableMetadata];
+        
+        if (![MetaMapButtonResult shared].delegate) {
+            [MetaMapButtonResult shared].delegate = self;
+        }
+        
         self->hasListeners = YES;
     });
 }
 
--(NSArray<NSString *> *)supportedEvents { return @[@"verificationSuccess", @"verificationCanceled"]; }
+RCT_EXPORT_METHOD(showFlowWithConfigurationId:(NSString * _Nonnull)clientId
+                  flowId:(NSString * _Nullable)flowId
+                  configurationId:(NSString * _Nullable)configurationId
+                  encryptionConfigurationId:(NSString * _Nullable)encryptionConfigurationId
+                  metadata:(NSDictionary<NSString *, id> * _Nullable)metadata)
+{
+    dispatch_async(dispatch_get_main_queue(), ^{
+        NSMutableDictionary *mutableMetadata = [metadata mutableCopy] ?: [NSMutableDictionary new];
+        mutableMetadata[@"sdkType"] = @"expo-ios";
+        
+        [MetaMap.shared showMetaMapFlowWithClientId:clientId
+                                             flowId:flowId
+                                    configurationId:configurationId
+                          encryptionConfigurationId:encryptionConfigurationId
+                                           metadata:mutableMetadata];
+        
+        if (![MetaMapButtonResult shared].delegate) {
+            [MetaMapButtonResult shared].delegate = self;
+        }
+        
+        self->hasListeners = YES;
+    });
+}
+
+- (NSArray<NSString *> *)supportedEvents {
+    return @[@"verificationSuccess", @"verificationCanceled"];
+}
 
 - (void)verificationSuccessWithIdentityId:(NSString *)identityId verificationID:(nullable NSString *)verificationID {
     if (hasListeners) {
-        [self sendEventWithName:@"verificationSuccess" body:@{@"identityId": identityId, @"verificationId": verificationID}];
+        [self sendEventWithName:@"verificationSuccess" body:@{@"identityId": identityId, @"verificationId": verificationID ?: [NSNull null]}];
     }
 }
 
 - (void)verificationCancelled {
     if (hasListeners) {
-        [self sendEventWithName:@"verificationCanceled" body: nil];
+        [self sendEventWithName:@"verificationCanceled" body:nil];
     }
 }
 
